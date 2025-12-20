9dfe12b31cd13da15e6e22b5a74b597315eb519c
Andy <62133915+cooloo9871@users.noreply.github.com>
2025-12-20 17:23:18 +0800

    Create image_push.sh

---
+++ image_push.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+# set -x
+
+RED='\033[1;31m' # alarm
+GRN='\033[1;32m' # notice
+YEL='\033[1;33m' # warning
+NC='\033[0m' # No Color
+registry="$1"
+
+
+[[ ! -f ./images.txt ]] && printf "${RED}images.txt file not found${NC}\n" && exit 1
+image_name=$(paste -sd' ' images.txt)
+
+
+docker_command() {
+
+tar --zstd -xf images.tar.zst
+
+sudo docker load < k8s_images.tar &>/dev/null
+
+while read line
+do
+  img="${line##*/}"
+  sudo docker tag $line $registry/library/$img &>/dev/null
+  sudo docker push $registry/library/$img &>/dev/null
+  if [[ "$?" == 0 ]]; then
+    printf "${GRN}push $registry/library/$img success${NC}\n"
+  else
+    printf "${RED}push $registry/library/$img fail${NC}\n"
+  fi
+done < images.txt
+}
+
+podman_command() {
+
+tar --zstd -xf images.tar.zst
+
+sudo podman load < k8s_images.tar &>/dev/null
+
+while read line
+do
+  img="${line##*/}"
+  sudo podman tag $line $registry/library/$img &>/dev/null
+  sudo podman push --tls-verify=false $registry/library/$img &>/dev/null
+  if [[ "$?" == 0 ]]; then
+    printf "${GRN}push $registry/library/$img success${NC}\n"
+  else
+    printf "${RED}push $registry/library/$img fail${NC}\n"
+  fi
+done < images.txt
+}
+
+help() {
+  cat <<EOF
+Usage: push.sh [harbor domain]
+
+for example: push.sh harbor.example.com
+EOF
+  exit
+}
+
+if ! which docker &>/dev/null && ! which podman &>/dev/null; then
+  printf "${RED}docker and podman command not found${NC}\n" && exit 1
+fi
+
+
+sudo -n true &>/dev/null
+if [[ "$?" != "0" ]]; then
+  printf "${RED}Passwordless sudo is NOT enabled${NC}\n" && exit 1
+fi
+
+if [[ "$#" < 1 ]]; then
+  help
+fi
+
+if which docker &>/dev/null; then
+  docker_command
+else
+  podman_command
+fi
